8cb54d536081dd8e02bbcda28605b63ce418b53b
F1nka0 <riainar@bk.ru>
2020-08-27 16:44:06 +0300

    Priorities has been added

--- Program.cs
+++ Program.cs
@@ -2,38 +2,41 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
-    
+
     class Program
     {
 
         private static string SourceString;
         private static Stack<int> Ints = new Stack<int>();
-        private static Stack<Dictionary<char,int>> Chars = new Stack<Dictionary<char, int>>();
-        private static void Parse(string PString) {
+        private static Stack<Dictionary<char, int>> Chars = new Stack<Dictionary<char, int>>();
+        private static void Parse(string PString)
+        {
 
-            foreach (char Current in PString) {
+            foreach (char Current in PString)
+            {
 
-                if (Priorities.Keys.Contains(Current)) {
+                if (Priorities.Keys.Contains(Current))
+                {
 
-                //    Chars.Push(new Dictionary<char, int>(Current,));
+                    //    Chars.Push(new Dictionary<char, int>(Current,));
                 }
-                else if (Enumerable.Range(0,10).Contains(int.Parse(Current.ToString()))) {
+                else if (Enumerable.Range(0, 10).Contains(int.Parse(Current.ToString())))
+                {
 
                     Ints.Push(int.Parse(Current.ToString()));
                 }
             }
         }
         private static Dictionary<char, int> Priorities;
-        static public void InitPriorities() {
+        static public void InitPriorities()
+        {
 
             Priorities = new Dictionary<char, int>();
-            Priorities.Add('(',1); //TODO 
-            Priorities.Add(')',1); //TODO
+            Priorities.Add('(', 1); //TODO 
+            Priorities.Add(')', 1); //TODO
             Priorities.Add('-', 1);
             Priorities.Add('+', 1);
             Priorities.Add('/', 2);
@@ -41,7 +44,6 @@ namespace Calculator
         }
         static void Main(string[] args)
         {
-            Console.WriteLine();
             Console.WriteLine(Directory.GetCurrentDirectory());
             InitPriorities();
 
@@ -49,7 +51,7 @@ namespace Calculator
             Console.WriteLine("Input");
             SourceString = Console.ReadLine();
 
-            
+
 
         }
     }
